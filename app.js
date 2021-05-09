ade170042de35ffe6a457e809df5986427f681c7
mligon115 <mligon115@yahoo.com>
2021-05-09 15:16:03 -0500

    Added code to track time and score. Working on centering the timeout image and getting the cards in random order.

--- app.js
+++ app.js
@@ -52,24 +52,55 @@
     ]
 
 
+
+
     const grid = document.querySelector('.grid')
     const timer = document.querySelector('#timer')
     const startBtn = document.querySelector('#start-button')
+    const resetBtn = document.querySelector('#reset')
+    const score = document.querySelector('#score')
     let clicked = []
     let clickedNum = []
     let matched = []
-    let time = 30
+    let time = 5
 
-   
+    // function shuffleCards(){
+    //     let newCard = 0
+    //     let tempCard = 0
 
+    //     for(let i = arr.length - 1; i > 0; i--){
+    //         newCard = Math.floor(Math.random() * (i + 1))
+    //         tempCard = arr[i]
+    //         arr[i] = arr[newCard]
+    //         arr[newCard] = tempCard
+    //     }
+    //     return arr
+    // }
+    
+
+
+    resetBtn.addEventListener('click', function(){
+        location.reload()
+    })
+  
+    //Timer
     function timeCountDown(){
         setInterval(function(){
             if(time <= 0){
                 clearInterval(time = 0)
+                return timeOut()
             }
             timer.innerHTML = `${time} seconds`
             time -= 1
+            
         }, 1000)
+    }
+    
+
+    function timeOut(){
+        document.querySelector('#timer').textContent= 'Time Out'
+        document.querySelector('.grid').innerHTML = " "
+        document.querySelector('.timeOut').src = 'images/Timeout.jpeg'
     }
 
     startBtn.addEventListener('click', timeCountDown)
@@ -77,43 +108,71 @@
     //create the board
     function gameBoard(){
         for(let i = 0; i < arr.length; i++){
-            var card = document.createElement('img')
+            let card = document.createElement('img')
             card.setAttribute('class', 'images')
             card.setAttribute('src', 'images/TheOffice.jpeg')
             card.setAttribute('data-index', i)
             card.addEventListener('click', cardImgDisplay)
             grid.appendChild(card)
         }
+        // shuffleCards()
     }
     gameBoard()
 
     //check to see if the cards match 
     function checkCard(){
-        // let card = this.getAttribute('data-index')
-        let cardNum1 = clicked[0]
-        let cardNum2 = clicked[1]
+        let cards = document.querySelectorAll('img')
+        let cardNum1 = clickedNum[0]
+       
+        let cardNum2 = clickedNum[1]
+       
 
-        if (cardNum1 === cardNum2){
-            alert('Match')
+        if (clicked[0] === clicked[1]){
+            cards[cardNum1].removeEventListener('click', cardImgDisplay)
+            
+            cards[cardNum2].removeEventListener('click', cardImgDisplay)
+           
             matched.push(clicked)
-            console.log(matched)
-        } else if (cardNum1 !== cardNum2) {
-            cardNum1 = clicked
-            console.log(cardNum1)
-            alert('Nope!')
+           
+        } else {
+            
+            cards[cardNum1].setAttribute('src','images/TheOffice.jpeg')
+            cards[cardNum2].setAttribute('src','images/TheOffice.jpeg')
+               
+           
+          
         } 
         clicked = []
-        clickedNum = []  
+        clickedNum = []
+        score.textContent = matched.length
+        if(matched.length === arr.length/2){
+            score.textContent = 'You Won!!!'
+          
+        }
+       
      }
 
+    //flip cards 
     function cardImgDisplay(){
         let cardImgNum = this.getAttribute('data-index')
-        clicked.push(arr[cardImgNum].name)
-        clickedNum.push(cardImgNum)
         this.setAttribute('src', arr[cardImgNum].img)
+      
+        
+        clicked.push(arr[cardImgNum].name)
+
+       
+        clickedNum.push(cardImgNum)
+      
+        
+        // clicked.length === 1 ? timeCountDown() : null
+        //     clearInterval(clicked.length === clicked)
+       
         if(clicked.length === 2){
+           
             setTimeout(() => checkCard(), 500)
+           
         }
+       
     }
 
 
